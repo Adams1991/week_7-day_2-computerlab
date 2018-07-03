b63cea26a50407930ad5e42f690de898e1a9ba41
Adams1991 <adams121@hotmail.co.uk>
2018-07-03 12:06:58 +0100

    Mouse set up and can send data

--- src/main/java/device_management/Mouse.java
+++ src/main/java/device_management/Mouse.java
@@ -1,4 +1,26 @@
 package device_management;
 
-public class Mouse {
+import behaviours.IInput;
+
+public class Mouse implements IInput{
+
+    private String type;
+    private String colour;
+
+    public Mouse(String type, String colour){
+        this.type = type;
+        this.colour = colour;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public String sendData(String data){
+        return "sending: " + data;
+    }
 }
--- src/test/java/MouseTest.java
+++ src/test/java/MouseTest.java
@@ -1,2 +1,30 @@
+import device_management.Mouse;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class MouseTest {
+
+    Mouse mouse;
+
+    @Before
+    public void before(){
+        mouse = new Mouse("cordless", "black");
+    }
+
+    @Test
+    public void hasType(){
+        assertEquals("cordless", mouse.getType());
+    }
+
+    @Test
+    public void hasColour(){
+        assertEquals("black", mouse.getColour());
+    }
+
+    @Test
+    public void canSendData(){
+        assertEquals("sending: clicks", mouse.sendData("clicks"));
+    }
 }
